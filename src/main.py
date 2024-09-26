dd83e7df51226de9ff42d29d79cb40573f9c77fe
= <firas.mahrooqi@outlook.com>
2024-09-26 12:40:52 +0400

    update after connecting docker to db

--- src/main.py
+++ src/main.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
 from routes import base, data
+from motor.motor_asyncio import AsynIOMotorClient
+from helpers.config import get_settings
 
 app = FastAPI()
+
+@app.on_event("startup")
+async def startup_db_client():
+
+    settings = get_settings()
+    app.mongo_conn = AsynIOMotorClient(settings.MONGODG_URL)
+    app.db_client = app.mongo_conn[settings.MONGODG_DATABASE]
+
+@app.on_event("shutdown")
+async def shutdown_db_client():
+    app.mongo_conn.close()
+
+
+
+
 app.include_router(base.base_router)
 app.include_router(data.data_router)
---
+++ src/models/db_schemes/project.py
@@ -0,0 +1,25 @@
+from pydantic import BaseModel , Field
+from typing import Optional
+from bson.objectid import ObjectId
+
+
+class Project(BaseModel): 
+    
+    #objectid is the type of how the mongodb consider the id string in the database; 
+    _id : Optional[ObjectId]
+    project_id : str = Field(... , min_length=1)
+
+
+# we use manual validator because "pydantic" is not familiar with type "ObjectId", and we need to do config class 
+    @validator('project_id')  
+    def validate_project_id (cls, value ):  #cls represent the class Project itself; 
+
+        if not value.isalnum(): 
+            raise ValueError("Value Must BE ALPHA NUMERIC")
+        
+        return value 
+    
+
+    class Config: 
+        arbitrary_types_allowed = True
+    
